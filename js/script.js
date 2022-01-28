2cc4abbd96c24c462c023631e3ce0b3e91a8c64f
Viorel Lazari <vlazari@gmail.com>
2022-01-28 17:30:41 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
 const cats = [
-    {
-        name: "Blob",
-        age: 10
-    },
-    {
-        name: "Harold",
-    },
-    {
-        name: "Blurt",
-        age: 21
-    }
+	{
+		name: "Blob",
+		age: 10,
+	},
+	{
+		name: "Harold",
+	},
+	{
+		name: "Blurt",
+		age: 21,
+	},
 ];
 
+// -- Question 1: -->
+
+const cat = { complain: catComplain() };
+function catComplain() {
+	console.log("Meow!");
+}
+
+// -- Question 2: -->
+// -- Question 3: -->
+// -- Question 4: -->
+// -- Question 5: -->
+// -- Question 6: -->
+// -- Question 7: -->
+// -- Question 8: -->
