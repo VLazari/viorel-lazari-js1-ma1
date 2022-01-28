1bee077fa02a110fe208a71affc8c50cdefcba3e
Viorel Lazari <vlazari@gmail.com>
2022-01-28 17:39:18 +0100

    question 3

--- js/script.js
+++ js/script.js
@@ -27,6 +27,9 @@ const heading = document.querySelector("h3");
 heading.innerHTML = `Updated heading`;
 
 // -- Question 3: -->
+
+heading.style.fontSize = "2em";
+
 // -- Question 4: -->
 // -- Question 5: -->
 // -- Question 6: -->
